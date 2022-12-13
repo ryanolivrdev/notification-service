eb7db006a01a8b739e593855e3651946801e95c5
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-13 15:24:56 -0300

    feat: get and post routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Voce tem uma nova solicitacao de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
