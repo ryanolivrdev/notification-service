1d6363b5fdbc5d76e0c7fe6c1c8bf1e586233bff
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 17:52:07 -0300

    feat: NotificationsRepository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { NotificationsRepository } from 'src/application/repositories/notification-repository';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaNotificationRepository } from './prisma/repositories/prisma-notifications-repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationRepository,
+    },
+  ],
+  exports: [NotificationsRepository],
 })
 export class DatabaseModule {}
