eff065526ccf084df01475d1d80e04e956520840
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 17:44:30 -0300

    feat: PrismaNotificationRepository connect database

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,8 +1,20 @@
-import { Notification } from 'src/application/entities/notification';
+import { Notification } from '../../../../application/entities/notification';
 import { NotificationsRepository } from '../../../../application/repositories/notification-repository';
+import { PrismaService } from './../prisma.service';
 
 export class PrismaNotificationRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
   async create(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
   }
 }
