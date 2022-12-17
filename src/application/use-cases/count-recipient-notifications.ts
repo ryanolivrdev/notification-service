c53ea89be89b175690015ede218274ef0b0e5d5e
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 20:57:14 -0300

    feat: create CountRecipientNotifications

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,30 @@
+import { NotificationNotFound } from './errors/notification-not-found';
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notification-repository';
+
+interface CountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationsRequest,
+  ): Promise<CountRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationsRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return {
+      count,
+    };
+  }
+}
