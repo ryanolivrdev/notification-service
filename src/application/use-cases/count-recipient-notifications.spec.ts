acbb028266d162f3f38508930edbd6c087759346
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 21:13:32 -0300

    feat: emplement factory makeNotification

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,5 +1,4 @@
-import { Content } from '@application/entities/content';
-import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from './../../../test/repositories/in-memory-notifications-repository';
 import { CountRecipientNotifications } from './count-recipient-notifications';
 
@@ -11,27 +10,15 @@ describe('Count recipients notifications', () => {
     );
 
     await notificationsRepository.create(
-      new Notification({
-        content: new Content('Você recebeu uma solicitação de amizade'),
-        category: 'social',
-        recipientId: 'recipient-1',
-      }),
+      makeNotification({ recipientId: 'recipient-1' }),
     );
 
     await notificationsRepository.create(
-      new Notification({
-        content: new Content('Você recebeu uma solicitação de amizade'),
-        category: 'social',
-        recipientId: 'recipient-1',
-      }),
+      makeNotification({ recipientId: 'recipient-1' }),
     );
 
     await notificationsRepository.create(
-      new Notification({
-        content: new Content('Você recebeu uma solicitação de amizade'),
-        category: 'social',
-        recipientId: 'recipient-2',
-      }),
+      makeNotification({ recipientId: 'recipient-2' }),
     );
 
     const { count } = await countNotification.execute({
