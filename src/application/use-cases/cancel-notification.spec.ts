058f5689b95326cb8baf0240aa0e17f7292e1451
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 21:05:22 -0300

    fix: name cancel notification test

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -5,7 +5,7 @@ import { CancelNotification } from './cancel-notification';
 import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('cancel notification', () => {
-  it('should be cancel a notification', async () => {
+  it('should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
