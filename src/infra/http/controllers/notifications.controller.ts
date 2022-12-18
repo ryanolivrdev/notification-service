963d80f842775908be94453481eb32b1b597a4f7
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 22:57:14 -0300

    style(NotificationsController): use path

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
-import { CancelNotification } from './../../../application/use-cases/cancel-notification';
-import { ReadNotification } from './../../../application/use-cases/read-notification';
-import { UnreadNotification } from './../../../application/use-cases/unread-notification';
-import { CountRecipientNotifications } from './../../../application/use-cases/count-recipient-notifications';
-import { GetRecipientNotifications } from './../../../application/use-cases/get-recipient-notifications';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
 import { NotificationViewModel } from './../view-models/notification-view-model';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
