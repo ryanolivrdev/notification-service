a419c8373e164f149080a3509cab4e73794b6d63
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-12-17 22:41:48 -0300

    feat(NotificationsController):  cancel route

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,29 @@
+import { CancelNotification } from './../../../application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from './../../../application/use-cases/count-recipient-notifications';
 import { NotificationViewModel } from './../view-models/notification-view-model';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
+
+  async counFromRecipiente() {}
+
+  async read() {}
+
+  async unread() {}
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
